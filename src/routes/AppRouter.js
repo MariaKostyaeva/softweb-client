80659af98f842d8c224454816e14095c3429232f
Мария Костяева <masha22.03@list.ru>
2023-03-27 16:07:14 +0300

    adjusting the project structure

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react';
 import {Routes,Route,Navigate} from "react-router-dom";
-import {adminRoutes, devRoutes, publicRoutes} from "../routes";
+import {adminRoutes, devRoutes, publicRoutes} from "../routes/routes";
 import {Context} from "../index";
 
 const AppRouter = () => {
